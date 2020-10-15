dc25c434187ca609d4d7588b9a92bfd40ed362ac
Andrew V. Jones <andrew.jones@vector.com>
2020-10-15 21:43:48 +0100

    include header
    Signed-off-by: Andrew V. Jones <andrew.jones@vector.com>

---
+++ func.h
@@ -0,0 +1,6 @@
+void bar(int x) {
+  ++x;
+  if (x)
+    ;
+  ++x;
+}
---
+++ main.c
@@ -0,0 +1,16 @@
+// comment
+// comment
+// comment
+// comment
+// comment
+// comment
+// comment
+
+#include "func.h"
+
+void foo(int x) {
+  ++x;
+  if (x)
+    ;
+  ++x;
+}
